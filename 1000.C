92295865b35a96475f4e2347a04e8dd46456334c
srushti <srushtisavsaviya24@gmail.com>
2023-06-28 17:40:39 +0530

    first commit

---
+++ 1000.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i=1;
+int n;
+clrscr();
+printf("enter any number: ");
+scanf("%d",&n);
+while(i<=10){
+  printf("%d\n",i);
+  i++;
+  }
+getch();
+}
